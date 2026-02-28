f442b8b8c8f8d80165505f639efa53977eaf5b7d
ajithaaa06 <harshithakb06@gmail.com>
2026-02-28 12:00:56 +0530

    ReverseNumber - 29/02/26

---
+++ ReverseNumber.java
@@ -0,0 +1,32 @@
+  import java.util.Scanner;
+    class Solution {
+    public int reverse(int x) {
+        int rev = 0;
+        while (x != 0) {
+            int pop = x % 10;
+            x /= 10;
+            if (
+                rev > Integer.MAX_VALUE / 10 ||
+                (rev == Integer.MAX_VALUE / 10 && pop > 7)
+            ) return 0;
+            if (
+                rev < Integer.MIN_VALUE / 10 ||
+                (rev == Integer.MIN_VALUE / 10 && pop < -8)
+            ) return 0;
+            rev = rev * 10 + pop;
+        }
+        return rev;
+    }
+}
+public class ReverseNumber {
+    public static void main(String [] args ){
+        Scanner sc = new Scanner (System.in);
+        Solution s = new Solution ();
+        System.out.println("Enter number to be reversed");
+        int x=sc.nextInt();
+         int rev =  s.reverse(x);
+        System.out.print(rev); 
+        sc.close();
+    }
+}
+
