68afe456da70c45c9d00f4446c6e81b0b7bdf044
ajithaaa06 <harshithakb06@gmail.com>
2026-03-04 21:36:06 +0530

    Fibonacci-4/3/26

---
+++ Fibonacci.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+class Fibonacci {
+
+    public static int fib(int n) {
+        if(n == 0)
+            return 0;
+        if(n == 1)
+            return 1;
+
+        int a = 0, b = 1, c = 0;
+
+        for(int i = 2; i <= n; i++) {
+            c = a + b;
+            a = b;
+            b = c;
+        }
+
+        return b;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter number");
+        int n = sc.nextInt();
+        System.out.println(fib(n));
+    }
+}
